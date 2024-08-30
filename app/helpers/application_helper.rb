86d8d20ff1bfbbd1edf132179313fb831a42826a
thuczige <thucpt@zigexn.vn>
2024-08-30 09:32:07 +0700

    init assets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def head_title
+    content_for(:title).present? ? "#{t(".base_title")} | #{content_for(:title)}" : t(".base_title")
+  end
 end
---
+++ config/importmap.rb
@@ -0,0 +1,2 @@
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "application", preload: true
