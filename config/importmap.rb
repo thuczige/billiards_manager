12ac83f7af263c336c933085513171107768e860
Phan Trong Thuc <161272669+thuczige@users.noreply.github.com>
2024-09-06 08:24:46 +0700

    update homepage content (#8)

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,2 +1,7 @@
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "application", preload: true
+# Pin npm packages by running ./bin/importmap
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "bootstrap", to: "bootstrap.min.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
