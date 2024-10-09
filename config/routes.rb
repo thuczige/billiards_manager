291fab4207981c6ebc6c5a456b4d11b496d3811a
Viet247 <viet24799@gmail.com>
2024-10-09 22:20:40 +0700

    back route file version

---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/, defaults: { locale: "vi" } do
+    # Defines the root path route ("/")
+    root "welcome#index"
+
+    get "signin" => "auth/session#new"
+    post "signin" => "auth/session#create"
+
+    get "signup" => "auth/registration#new"
+    post "signup" => "auth/registration#create"
+
+    get "forgot_password" => "auth/recoverable#new"
+    post "forgot_password" => "auth/recoverable#create"
+  end
+end
